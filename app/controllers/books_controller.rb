6ce7193c8ec7e341cb9b48d94a14e9b5e5f4e4e7
tajimakensuke <kenyuu25@gmail.com>
2021-03-28 01:04:38 +0000

    [add]routes

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,18 @@
+class BooksController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'post_books#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resource :post_books, only: [:new, :create, :index, :show, :destroy]
+  resource :user, only: [:show, :edit, :update]
 end
